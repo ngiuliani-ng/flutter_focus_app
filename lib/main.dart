690fe52bd1552865db1faddac8c9649bef701163
Nicolò Giuliani <info@ngiuliani.com>
2021-04-17 08:41:39 +0200

    Update.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:flutter_focus_app/pages/auth/splashPage.dart';
+import 'package:flutter_focus_app/pages/auth/splash.dart';
 
 void main() {
   runApp(App());
